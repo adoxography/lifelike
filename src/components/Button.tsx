9633e87ac2f7d76d84bd1e7b10b8cfd11c7ffb2b
Graham Still <graham@gstill.dev>
2022-10-02 17:47:56 -0500

    Focus states

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,8 @@ const getColorClasses = (variant: string) => {
 
 const Button = ({ children, className = '', variant, ...props }) => (
   <button className={`
-      relative py-2 px-6 transition-colors rounded-sm uppercase box-shadow-sm shadow-highlight tracking-wide
+      relative py-2 px-6 transition-colors rounded-sm uppercase drop-shadow-sm shadow-highlight tracking-wide
+      focus-visible:outline-none focus-visible:ring-1 ring-sky-300/50
       after:absolute after:top-0 after:left-0 after:w-full after:h-full
       after:bg-gradient-to-br after:from-transparent after:to-slate-900/40
       ${getColorClasses(variant)} ${className}
